404ede9223407f00b5f34e1dc3d130829d2d2d1c
Santiago Castaneda Ramos <santiago.castaneda@globant.com>
2019-12-18 11:54:02 -0600

    HTTP Requests & Updating State

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar  from './components/layout/Navbar';
 import Users  from './components/users/Users';
+import  axios from 'axios';
 
 class App extends Component {
+
+  state = {
+    users: [],
+    loading: false,
+  }
+
+  async componentDidMount() {
+    this.setState({loading: true});
+    const resp =  await axios.get('https://api.github.com/users');
+    this.setState({users: resp.data, loading: false})
+    console.log(resp.data);
+  }
 
   render() {  
 
@@ -11,7 +24,7 @@ class App extends Component {
       <div className="App">
         <Navbar icon="fa fa-github" title="Github Finder" />
         <div className="container">
-        <Users />
+        <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </div>
     );
