24d08dc3d38d2241f75068c2371a58d14a84425c
santiago castañeda ramos <santiago.castaneda@globant.com>
2019-12-30 13:36:08 -0600

    created reducer and actions to setLoading and SearchUsers

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -20,7 +20,19 @@ const GithubState = props => {
 
   const [state, dispatch] = useReducer(GithubReducer, initalState);
 
-  //Searcg Users
+  //Search Users
+
+  const searchUsers = async text => {
+    setLoading();
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    dispatch({
+      type: SEARCH_USERS,
+      payload: res.data.items
+    });
+  };
 
   //Get User
 
@@ -30,13 +42,16 @@ const GithubState = props => {
 
   // Set Loading
 
+  const setLoading = () => dispatch({ type: SET_LOADING });
+
   return (
     <GithubContext.Provider
       value={{
         users: state.users,
         user: state.user,
         repos: state.repos,
-        loading: state.loading
+        loading: state.loading,
+        searchUsers
       }}
     >
       {props.children}
